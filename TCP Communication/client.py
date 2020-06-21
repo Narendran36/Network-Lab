e2f0c4bffe26ab47b66cd7de74191c26ab6e99d3
Narendran S Nair <unninarendran36@gmail.com>
2020-06-21 20:04:31 +0530

    Update client.py

--- TCP Communication/client.py
+++ TCP Communication/client.py
@@ -5,7 +5,8 @@ PORT = 65432        # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
-
-print('Received', repr(data))
+    while True:
+    	msg = input().encode()
+    	s.sendall(msg)
+    	data = s.recv(1024).decode()
+    	print('Received', repr(data))
